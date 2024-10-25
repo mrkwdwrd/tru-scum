dbf1f258e9000dfdc171f1481bb5e8c92c860c4f
Mark Woodward <markw@digitalbridge.com.au>
2024-10-25 17:38:21 +1100

    sizing and positioning

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,18 @@ onMounted(() => {
 
 <template>
   <Camera v-show="false" />
-  <Scene v-if="scene === 1" :id="1" />
-  <Scene v-if="scene === 2" :id="2" />
+  <main>
+    <Scene v-if="scene === 1" :id="1" />
+    <Scene v-if="scene === 2" :id="2" />
+  </main>
 </template>
 
 <style scoped>
+  main {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+  }
 </style>
--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -4,7 +4,28 @@ const props = defineProps({
   id: Number
 })
 </script>
+
 <template>
-  <p>Scene: {{ id }}</p>
-  <Camera :id="`s${id}c1`"/>
+  <section>
+    <div class="debug">Scene: {{ id }}</div>
+    <Camera :id="`s${id}c1`"/>
+  </section>
 </template>
+
+<style scoped>
+  section {
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
+  .debug {
+    position: absolute;
+    background: #000;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    color: #666;
+    padding: 5px 10px;
+    font-size: 11px;
+  }
+</style>
