925cf5455a1107732422b46c71f71635a68fc7ce
Mark Woodward <markw@digitalbridge.com.au>
2024-10-25 16:31:17 +1100

    show/hide camera on keydown

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
 import Camera from '@/components/Camera.vue'
+import { onMounted, ref } from 'vue';
+
+const showCamera = ref(false)
+
+const keyListener = (e) => {
+  showCamera.value = !showCamera.value
+}
+
+onMounted(() => {
+  document.addEventListener('keydown', keyListener)
+})
 </script>
 
 <template>
-  <Camera id="cam-1"/>
+  <Camera id="cam-1" v-if="showCamera"/>
 </template>
 
 <style scoped>
