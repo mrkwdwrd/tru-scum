6e4b878ed6007d11944ef5d6d5e16ffb241c61b4
Mark Woodward <markw@digitalbridge.com.au>
2024-10-25 17:50:15 +1100

    scenes with slots

---
+++ src/scenes/SceneOne.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Scene  from '@/components/Scene.vue';
+  import Camera from '@/components/Camera.vue'
+
+  const props = defineProps({
+    id: Number
+  })
+</script>
+
+<template>
+  <Scene :id="id">
+    <Camera :id="`s${id}c1`"/>
+  </Scene>
+</template>
+
+<style scoped>
+</style>
