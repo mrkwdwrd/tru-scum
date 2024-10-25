ce2aefab8887d646ff718c85ff10840b03c453c0
Mark Woodward <markw@digitalbridge.com.au>
2024-10-25 16:17:44 +1100

    stream local webcam

---
+++ src/components/Camera.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue';
+
+const startCamera = () => {
+    const video = document.getElementById('video')
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then((stream) => {
+              video.srcObject = stream
+          })
+          .catch(function(error) {
+              console.log("Something went wrong!")
+          });
+    }
+}
+
+onMounted(() => {
+  startCamera()
+})
+</script>
+
+<template>
+  <video id="video" autoplay></video>
+</template>
+
+<style scoped>
+</style>
