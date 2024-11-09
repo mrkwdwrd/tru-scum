0c279ebc00e74160d1b0fabe8c868e497a028a5e
Mark Woodward <markw@digitalbridge.com.au>
2024-11-09 23:47:19 +1000

    ig comments

---
+++ src/components/IgAvatar.vue
@@ -0,0 +1,10 @@
+<script setup>
+  const props = defineProps({
+    stories: Boolean
+  })
+</script>
+<template>
+  <span class="rounded-full h-10 w-10 p-[0.2em] border flex-shrink-0 border-gray-400" :class="[{ 'border-pink-500': stories }]">
+    <i class="inline-block h-8 w-8 bg-white rounded-full "></i>
+  </span>
+</template>
