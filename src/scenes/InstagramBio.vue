b7d0112ecb30d13d4d59bc58423af918af6a2787
Mark Woodward <markw@digitalbridge.com.au>
2024-11-16 23:15:01 +1100

    new juniper pics, missing scenes

---
+++ src/scenes/InstagramBio.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Scene from '@/components/Scene.vue'
+  import IgBio from '@/components/IgBio.vue'
+  import profileImage from '@/assets/images/openart-image_GjENJ-VC_1731748862428_raw.png'
+
+  const props = defineProps({
+  })
+
+</script>
+<template>
+  <Scene class="bg-black max-w-[1980px] mx-auto">
+      <div class="flex h-full relative">
+        <main class="w-full flex items-center">
+          <IgBio class="scale-150" :image="profileImage"/>
+        </main>
+      </div>
+  </Scene>
+</template>
