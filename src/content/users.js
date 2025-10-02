83e8a46cc9f46fe74e583798ca03fcf2fda119af
Mark Woodward <mark@markwoodward.com.au>
2025-10-02 23:49:47 +1000

    tweet updates

--- src/content/users.js
+++ src/content/users.js
@@ -1,18 +1,21 @@
 export const twitter = {
   amelia: {
     name: 'Amelia Collins',
+    verified: true,
     handle: '@aecollins76',
-    bio: '',
-    avatar: '/src/assets/users/twitter/jk.jpg'
+    bio: 'Real women only.',
+    avatar: null
   },
   greg: {
     name: 'The Logic Trap',
+    verified: true,
     handle: '@logictrappod',
     bio: '',
-    avatar: '/src/assets/users/twitter/jk.jpg'
+    avatar: '/src/assets/users/twitter/LogicTrap_logo.jpg'
   },
   jk: {
-    name: 'JK Rowling',
+    name: 'J.K. Rowling',
+    verified: true,
     handle: '@jk_rowling',
     bio: 'Writer sometimes known as Robert Galbraith',
     avatar: '/src/assets/users/twitter/jk.jpg'
