40b401fe5e0252fbaa0ad7afbeae3d5308839f09
Mark Woodward <mark@markwoodward.com.au>
2025-10-07 00:16:13 +1100

    add horizontals to grid

--- src/Grid.vue
+++ src/Grid.vue
@@ -6,7 +6,9 @@
 <template>
   <Scene class="bg-transparent max-w-[1980px] mx-auto top-0 right-0 left-0 !absolute">
     <div class="flex h-full justify-between border border-green-500">
-      <div v-for="i in 12" class="border-r border-green-500" />
+      <div v-for="i in 12" class="border-r border-green-500 flex flex-col justify-between h-full w-full items-stretch">
+        <div v-for="i in 12" class="border-b border-green-500 w-full h-full" />
+      </div>
     </div>
   </Scene>
 </template>
