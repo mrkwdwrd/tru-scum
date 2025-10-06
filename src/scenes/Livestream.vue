d6bbd0a14a0546740b605d48c2bc9b2b6430c156
Mark Woodward <mark@markwoodward.com.au>
2025-10-07 00:21:36 +1100

    remove unused livestream components

--- src/scenes/Livestream.vue
+++ src/scenes/Livestream.vue
@@ -1,19 +1,23 @@
 <script setup>
   import Scene  from '@/components/Scene.vue'
   import Camera from '@/components/Camera.vue'
+  import Comments from '@/components/Comments.vue'
   import Caption from '@/components/LivestreamCaption.vue'
   import StaticStream from '@/components/StaticStream.vue'
-  import { getCurrentInstance, onMounted, ref } from 'vue'
+
+  import { onMounted, getCurrentInstance, ref } from 'vue'
 
   const instance = getCurrentInstance()
 
   const props = defineProps({
-    live:  Boolean,
-    time: Number,
-    viewers: Number
+    live: Boolean,
+    existingComments: Array,
+    comments: Array,
+    viewers: Number,
+    time: Number
   })
 
-  const emits = defineEmits(['count', 'mount'])
+  const emits = defineEmits(['count', 'mount', 'comments'])
 
   const uuid = ref(instance.uid)
 
@@ -24,14 +28,19 @@
 
 <template>
   <Scene class="bg-black max-w-[1980px] mx-auto">
-    <div class="flex h-full w-3/4 mx-auto items-center">
-      <div class="w-full p-5 flex items-center flex-col">
+    <div class="flex h-full items-center ">
+      <div class="w-2/3 p-5 flex items-center flex-col"
+        style="transform: translate(100px, 140px)">
         <Camera v-if="live" :id="uuid" :footer="true" :time="time">
           <Caption :viewers="viewers" />
         </Camera>
         <StaticStream v-else :id="uuid" :footer="true" :time="time">
           <Caption :viewers="viewers" />
         </StaticStream>
+      </div>
+      <div class="h-3/4 w-1/3 flex items-center justify-center pl-36 relative overflow-hidden">
+        <Comments :existingComments="existingComments" :comments="comments" :time="time" @comments="val => emits('comments', val)"/>
+        <div class="absolute top-0 left-36 right-0 h-1/3 bg-gradient-to-t from-transparent to-black" />
       </div>
     </div>
   </Scene>
